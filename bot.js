445345857186ac292d700c87c73820fa6d668b7d
lawnxyldo <45184007+lawnxyldo@users.noreply.github.com>
2018-11-24 21:49:12 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,19 +1,15 @@
-/*
- * Discord Bot Builder Bot
- * Version 1.2.0
- * Robert Borghese
- */
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-const Files = require(require('path').join(__dirname, 'js', 'Main.js')).Files;
-
-if(!process.send) {
-
-Files.initStandalone();
-
-} else {
-
-process.on('message', function(content) {
-	Files.initBotTest(content);
+client.on('ready', () => {
+    console.log('I am ready!');
 });
 
-}
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
+});
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
