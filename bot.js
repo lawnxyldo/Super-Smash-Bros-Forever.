3a25c59115c1a548bcbb8cc1a926d1267491bc4c
lawnxyldo <45184007+lawnxyldo@users.noreply.github.com>
2018-11-24 23:50:26 +0100

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,15 +1,19 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+/*
+ * Discord Bot Builder Bot
+ * Version 1.2.0
+ * Robert Borghese
+ */
 
-client.on('ready', () => {
-    console.log('I am ready!');
+const Files = require(require('path').join(__dirname, 'js', 'Main.js')).Files;
+
+if(!process.send) {
+
+Files.initStandalone();
+
+} else {
+
+process.on('message', function(content) {
+	Files.initBotTest(content);
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
-});
-
-// THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+}
